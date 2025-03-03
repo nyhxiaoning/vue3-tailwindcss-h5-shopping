ddfedd3cb8e4d8741b4b6601374dfbf2b99d2ff4
Henry <34833782+nyhxiaoning@users.noreply.github.com>
2025-03-03 13:14:46 +0800

    Update main.ts
    引入默认样式：normalize.css

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import piniaPersistedstate from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
+import './normalize.css'
 import './index.css'
 import { useVant } from './hooks'
 
